27e1ba5fa9629000305c823aa519de0563ec2d2d
pratikrahane01 <pratikrahane66@gmail.com>
2026-01-16 13:08:04 +0530

    Update store_data_visualization_using_seaborn.py

--- store_data_visualization_using_seaborn.py
+++ store_data_visualization_using_seaborn.py
@@ -19,6 +19,7 @@ print(df.isnull().sum())
 sns.set(style="darkgrid")
 sns.barplot(x="City",y="Sales",data=df,hue="Category",palette="viridis")
 plt.title("Distribution of Sales by City")
+plt.savefig("barplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -27,6 +28,7 @@ plt.show()
 sns.set(style="darkgrid")
 sns.histplot(x="Sales",data=df,kde="True",hue="Category",bins=40,palette="deep")
 plt.title("Distribution of Sales by Category")
+plt.savefig("histplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -37,6 +39,7 @@ sns.kdeplot(x="Sales",data=df,hue="Region",palette="mako",linestyle=":")
 plt.xlabel("profit")
 plt.ylabel("Density")
 plt.title("Sales Density by Region")
+plt.savefig("kdeplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -45,6 +48,7 @@ plt.show()
 sns.set(style="dark")
 sns.boxplot(x="Category",y="Profit",data=df,hue="Region",palette="plasma")
 plt.title("Profit Distribution by Category and Region")
+plt.savefig("boxplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -52,6 +56,7 @@ plt.show()
 
 sns.violinplot(x="Category",y="Sales",data=df,hue="Segment",split=True,palette="viridis")
 plt.title("Sales Distribution by Category and Segment")
+plt.savefig("violinplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -60,24 +65,28 @@ plt.show()
 sns.boxplot(data=df, x="Category", y="Profit", color="lightgray")
 sns.stripplot(data=df, x="Category", y="Profit", jitter=True)
 plt.title("Profit Distribution with Actual Points")
+plt.savefig("stripplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
 #jointplot
 
 sns.jointplot(x="Sales",y="Profit",data=df,kind="hex",palette="deep")
+plt.savefig("jointplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
 #pairplot
 
 sns.pairplot(df[["Sales","Profit","Discount","Quantity","Category"]], hue="Category",palette="deep",kind="scatter",diag_kind="kde")
+plt.savefig("pairplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
 #relplot
 
 sns.relplot(data=df, x="Sales", y="Profit", hue="Category", col="Region")
+plt.savefig("relplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -86,6 +95,7 @@ plt.show()
 pivot = df.pivot_table(values="Sales", index="Category", columns="Region", aggfunc="mean")
 sns.heatmap(pivot, annot=True, fmt=".0f",cmap="Accent")
 plt.title("Average Sales by Category and Region")
+plt.savefig("Heatmaplot.png",dpi=300,bbox_inches="tight")
 plt.show()
 
 
@@ -98,4 +108,6 @@ monthly["Order Date"] = monthly["Order Date"].astype(str)
 sns.lineplot(data=monthly, x="Order Date", y="Sales", hue="Category")
 plt.xticks(rotation=45)
 plt.title("Monthly Sales Trend by Category")
+plt.savefig("lineplot.png",dpi=300,bbox_inches="tight")
 plt.show()
+
